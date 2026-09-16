7423d6cabc8454863dd9c39a611906492a191af9
laualways <180106141+laualways@users.noreply.github.com>
2026-09-16 12:03:39 +0100

    Add resource checking and updating logic for all drinks

--- day-15-coffee-machine/main.py
+++ day-15-coffee-machine/main.py
@@ -1,1 +1,37 @@
-from game_data import MENU
+from game_data import MENU, resources
+
+coffe_machine_on = True
+
+while coffe_machine_on:
+    drink_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+
+    if drink_choice == "espresso":
+        # water_resources = resources['water']
+        # menu_espresso_water = MENU['espresso']['ingredients']['water']
+        if resources['water'] > MENU['espresso']['ingredients']['water'] and resources['coffee'] > MENU['espresso']['ingredients']['coffee']:
+            resources['water'] -= MENU['espresso']['ingredients']['water']
+            resources['coffee'] -= MENU['espresso']['ingredients']['coffee']
+            print(resources)
+
+    if drink_choice == "latte":
+        # water_resources = resources['water']
+        # menu_espresso_water = MENU['latte']['ingredients']['water']
+        if resources['water'] > MENU['latte']['ingredients']['water'] and resources['coffee'] > MENU['latte']['ingredients']['coffee'] and resources['milk'] > MENU['latte']['ingredients']['milk']:
+            resources['water'] -= MENU['latte']['ingredients']['water']
+            resources['coffee'] -= MENU['latte']['ingredients']['coffee']
+            resources['milk'] -= MENU['latte']['ingredients']['milk']
+            print(resources)
+
+    if drink_choice == "cappuccino":
+        # water_resources = resources['water']
+        # menu_espresso_water = MENU['cappuccino']['ingredients']['water']
+        if resources['water'] > MENU['cappuccino']['ingredients']['water'] and resources['coffee'] > MENU['cappuccino']['ingredients']['coffee'] and resources['milk'] > MENU['cappuccino']['ingredients']['milk']:
+            resources['water'] -= MENU['cappuccino']['ingredients']['water']
+            resources['coffee'] -= MENU['cappuccino']['ingredients']['coffee']
+            resources['milk'] -= MENU['cappuccino']['ingredients']['milk']
+            print(resources)
+
+
+    if drink_choice == "off":
+        coffe_machine_on = False
